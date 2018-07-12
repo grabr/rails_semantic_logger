ba2b7d99eaeb10647ebade5ac825006e9e183916
konukhov <me@thdr.io>
2018-07-12 16:56:12 +0300

    do not monkeypatch activejob

--- lib/rails_semantic_logger/extensions/active_job/logging.rb
+++ lib/rails_semantic_logger/extensions/active_job/logging.rb
@@ -1,14 +1,13 @@
 # Patch ActiveJob logger
 require 'active_job/logging'
 
-module ActiveJob
-  module Logging
-    include SemanticLogger::Loggable
+module SemanticLoggerExtension
+  private
 
-    private
-
-    def tag_logger(*tags, &block)
-      logger.tagged(*tags, &block)
-    end
+  def tag_logger(*tags, &block)
+    logger.tagged(*tags, &block)
   end
 end
+
+ActiveJob::Logging.send :include, SemanticLogger::Loggable
+ActiveJob::Logging.send :prepend, SemanticLoggerExtension
